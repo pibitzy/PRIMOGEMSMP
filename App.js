7a5fdb13b0f5ce32f3d0188bc0ff956575547672
MikelBenciGitHub <129713897+MikelBenciGitHub@users.noreply.github.com>
2023-05-26 10:41:46 +0700

    Update App.js
    Update baru:
    Selain mendapatkan list album, sekarang hasil search juga akan menampilkan track dari artist yang dicari. Selain itu, akan muncul jendela pop up yang menampilkan informasi mengenai album atau track yang di klik oleh user.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "../public/styles.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card, Button, Row, Container } from "react-bootstrap";
+import { Card, Button, Row, Col, Container, Modal } from "react-bootstrap";
 
 const clientID = "eeef045c75e5488f8543305bc2be05f4";
 const clientSecret = "29882680ac4c4d0fb9ae342280a7101c";
@@ -9,7 +9,10 @@ const clientSecret = "29882680ac4c4d0fb9ae342280a7101c";
 export default function App() {
   const [searchingInput, setSearchingInput] = useState("");
   const [token, setToken] = useState("");
+  const [tracks, setTracks] = useState([]);
   const [albums, setAlbums] = useState([]);
+  const [selectedAlbum, setSelectedAlbum] = useState(null);
+  const [selectedTrack, setSelectedTrack] = useState(null);
 
   // API interaction
   useEffect(() => {
@@ -55,7 +58,7 @@ export default function App() {
       "https://api.spotify.com/v1/artists/" +
         artistID +
         "/albums" +
-        "?include_groups=album&market=US&limit=50",
+        "?include_groups=album&limit=50",
       searchP
     )
       .then((response) => response.json())
@@ -64,45 +67,141 @@ export default function App() {
         setAlbums(data.items);
       });
 
-    console.log(albums);
-    // Display
+    var tracksResult = await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/top-tracks" +
+        "?include_groups=tracks&market=US&limit=50",
+      searchP
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTracks(data.tracks);
+      });
   }
+
+  // Open modal for album details
+  const openAlbumModal = (album) => {
+    setSelectedAlbum(album);
+  };
+
+  // Open modal for track details
+  const openTrackModal = (track) => {
+    setSelectedTrack(track);
+  };
+
+  // Close modal
+  const closeModal = () => {
+    setSelectedAlbum(null);
+    setSelectedTrack(null);
+  };
 
   return (
     <div className="App">
       {/* Search Section */}
       <Container>
         <input
-          placeholder="Search for Artist"
+          placeholder="Search by Artist"
           type="input"
           onChange={(event) => {
             setSearchingInput(event.target.value);
           }}
           onKeyPress={(event) => {
-            if (event.key == "Enter") {
+            if (event.key === "Enter" && searchingInput !== "") {
               // Jika sudah tekan enter, lakukan searching sesuai input
               search();
             }
           }}
         />
 
-        <Button onClick={search}>Search</Button>
+        <Button
+          onClick={() => {
+            if (searchingInput !== "") {
+              search();
+            }
+          }}
+        >
+          Search
+        </Button>
       </Container>
       {/* Album Section */}
       <Container>
         <Row className="mx-2 row row-cols-4">
-          {albums.map((album, i) => {
-            return (
-              <Card>
-                <Card.Img src={album.images[0].url} />
-                <Card.Body>
-                  <Card.Title>{album.name}</Card.Title>
-                </Card.Body>
-              </Card>
-            );
-          })}
+          <Col className="col-sm-6">
+            <h1>Albums</h1>
+            {albums.map((album, i) => {
+              return (
+                <Card key={i} onClick={() => openAlbumModal(album)}>
+                  <Card.Img src={album.images[0].url} />
+                  <Card.Body>
+                    <Card.Title>{album.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Col>
+          <Col className="col-sm-6">
+            <h1>Tracks</h1>
+            {tracks.map((track, i) => {
+              return (
+                <Card key={i} onClick={() => openTrackModal(track)}>
+                  <Card.Img src={track.album.images[0].url} />
+                  <Card.Body>
+                    <Card.Title>{track.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Col>
         </Row>
       </Container>
+
+      {/* Album pop up window when selected */}
+      <Modal show={selectedAlbum !== null} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Album Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedAlbum && (
+            <div>
+              <h2>Album Name: {selectedAlbum.name}</h2>
+              <h2>Release Date: {selectedAlbum.release_date}</h2>
+              <h2>Tracks Total: {selectedAlbum.total_tracks}</h2>
+              <h2>Artist : {selectedAlbum.artists[0].name}</h2>
+              {/* Display additional album details here */}
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Track pop up window when selected */}
+      <Modal show={selectedTrack !== null} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Track Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedTrack && (
+            <div>
+              <h2>Track Name: {selectedTrack.name}</h2>
+              <h2>Track Album: {selectedTrack.album.name}</h2>
+              <h2>Release Date: {selectedTrack.album.release_date}</h2>
+              <h2>Artist : {selectedTrack.artists[0].name}</h2>
+              {/* Display additional track details here */}
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
